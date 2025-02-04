b67b7f59304bfaaab475c27e5433bed4f6420eeb
DevSeulgi <DevSeulgi@gmail.com>
2025-02-04 22:37:31 +0900

    feat: 전달받은 데이터로 이미지 리소스 변경

--- 03-splash-screen-and-data-passing/app/src/main/java/com/dino/splash_screen_and_data_passing/ImageInsideActivity.kt
+++ 03-splash-screen-and-data-passing/app/src/main/java/com/dino/splash_screen_and_data_passing/ImageInsideActivity.kt
@@ -1,6 +1,7 @@
 package com.dino.splash_screen_and_data_passing
 
 import android.content.Intent
+import android.media.Image
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.Toast
@@ -16,7 +17,37 @@ class ImageInsideActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_image_inside)
 
+        val memberImage = findViewById<ImageView>(R.id.memberImageArea)
+
         val getData = intent.getStringExtra("data")
-        Toast.makeText(this, getData, Toast.LENGTH_LONG).show()
+        // Toast.makeText(this, getData, Toast.LENGTH_LONG).show()
+
+        if(getData == "1"){
+            memberImage.setImageResource(R.drawable.member_1)
+        }
+        if(getData == "2"){
+            memberImage.setImageResource(R.drawable.member_2)
+        }
+        if(getData == "3"){
+            memberImage.setImageResource(R.drawable.member_3)
+        }
+        if(getData == "4"){
+            memberImage.setImageResource(R.drawable.member_4)
+        }
+        if(getData == "5"){
+            memberImage.setImageResource(R.drawable.member_5)
+        }
+        if(getData == "6"){
+            memberImage.setImageResource(R.drawable.member_6)
+        }
+        if(getData == "7"){
+            memberImage.setImageResource(R.drawable.member_7)
+        }
+        if(getData == "8"){
+            memberImage.setImageResource(R.drawable.member_8)
+        }
+        if(getData == "9"){
+            memberImage.setImageResource(R.drawable.member_9)
+        }
     }
 }
