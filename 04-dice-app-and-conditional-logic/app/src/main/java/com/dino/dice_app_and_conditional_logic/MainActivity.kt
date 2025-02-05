dc6a66d059a174fd5e217579a329b6a640d046d7
DevSeulgi <DevSeulgi@gmail.com>
2025-02-05 23:18:12 +0900

    feat: dice 앱 프로젝트 생성

---
+++ 04-dice-app-and-conditional-logic/app/src/main/java/com/dino/dice_app_and_conditional_logic/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.dino.dice_app_and_conditional_logic
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.testBtnId)
+
+    }
+}
